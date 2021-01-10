6777e4dfae467eb37891905239ff0ce712fd1e8f
David Rieser <david.w.rieser@gmail.com>
2021-01-10 11:47:47 +0100

    Syntax Changes for less unnecessary Signals

--- ALU_Subcircuits/Subtractor.vhdl
+++ ALU_Subcircuits/Subtractor.vhdl
@@ -18,12 +18,8 @@ end Subtractor;
 architecture behaviour of Subtractor is
 
     signal carry    : std_logic_vector(regWidth - 1 downto 0);
-	signal temp_out : std_logic_vector(regWidth - 1 downto 0);
+    signal intern_add 	: std_logic_vector(regWidth - 1 downto 1);
     signal twosComplement : std_logic_vector(regWidth - 1 downto 0);
-
-	signal negative : std_logic;
-	signal zero		: std_logic;
-	signal zero_temp: std_logic_vector(regWidth - 1 downto 0);
 
 	begin
 
@@ -39,25 +35,18 @@ architecture behaviour of Subtractor is
         -- First Full-Adder has to be created outside of Generate-Statement
         carry(0)    <= (not(inputA(0) xor twosComplement(0)) and inputA(0)) or
 						((inputA(0) xor twosComplement(0)) and carryIn);
-		zero_temp(0)<= temp_out(0);
 
         Adders : for i in 1 to regWidth - 1 generate
 
-            signal z : std_logic;
-
             begin
 
-                z          	<= inputA(i) xor twosComplement(i);
-                carry(i)    <= (not(z) and inputA(i)) or (z and carry(i - 1));
-                temp_out(i) <= z xor carry(i - 1);
-				zero_temp(i)<= temp_out(i) or zero_temp(i - 1);
+                intern_add(i)	<= inputA(i) xor twosComplement(i);
+                carry(i)    	<= (not(intern_add(i)) and inputA(i)) or (intern_add(i) and carry(i - 1));
+                aOutput(i) 		<= intern_add(i) xor carry(i - 1);
 
         end generate Adders;
 
-		zero 			<= not zero_temp(regWidth - 1);
-		negative		<= carry(regWidth - 1) and zero_temp(regWidth - 1);
-		aCarry			<= negative;
-        temp_out(0)  	<= (inputA(0) xor inputB(0)) xor carryIn;
-		aOutput			<= temp_out;
+		aCarry			<= not carry(regWidth - 1);
+		aOutput(0)  	<= (inputA(0) xor inputB(0)) xor carryIn;
 
 end behaviour;
